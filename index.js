df656ea96fe6df87ad30ed25e2d72141df5b9401
jacobsilver2 <jacobsilver2@mac.com>
2018-05-08 09:51:27 -0400

    still

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 function produceDrivingRange(blockRange) {
   return function (start, end) {
-    const diff = start - end;
-    if 
+    const distance = start - end;
+    const diff = blockRange - distance;
+    
+    if (diff > 0 ) {
+      `within range by ${diff}`
+    } else {
+      `${diff} blocks out of range`
+    }
   }
 }

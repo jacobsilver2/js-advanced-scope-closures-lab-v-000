194f2f4f0dda5c86e089463ce5d8b10ae0cd4f10
jacobsilver2 <jacobsilver2@mac.com>
2018-05-08 09:52:18 -0400

    why

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 function produceDrivingRange(blockRange) {
   return function (start, end) {
-    const distance = start - end;
+    
+    const distance = end - start;
     const diff = blockRange - distance;
     
     if (diff > 0 ) {
